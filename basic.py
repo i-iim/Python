a375040bf259c406e7513b5495322058ae1a5fed
i-iim <102364834+i-iim@users.noreply.github.com>
2023-11-04 16:42:46 +0100

    learned new stuff

---
+++ basic.py
@@ -0,0 +1,52 @@
+name = "Max"
+#variabeln
+#Unerlaubt: keywörter, mit:%,/,...
+
+name = " Max"; print(name)
+#mit semikolon mehrere aussagen in einer linie
+
+#Kommentararten: mit `#` oder mit """ """ über mehrere Zeilen
+
+#Datentypen: mit == vergleich, mit isisntance kann man Datentyp herrausfinden
+#Datentypen, welche gibt es??
+
+number = "20"
+age = int(number)
+#Casten von Datentypen:
+
+
+#Opperatoren: =, +, -, *, /, %, ** (Quadrat), // (Flordivision, abrunden auf Int)
+#Weitere z.B.: "+=" -> check check
+#Vergelch: ==, !=, <, <=, ...
+
+#Boolen opperatoren: not var -> umdrehen, var and var, var or var
+# and, or -> Bit
+
+#kann auch in retrun schreiben
+#return True if age>18 else False
+
+string_1= "HELLO"
+string_2 = "WORLD!"
+string_3 = string_1 + " " + string_2
+print(string_3)
+
+print(""" 
+      
+Das ist ein Print über mehrere
+Zeilen
+
+      """)
+
+#ansonsten viele String methoden intuitiv, geben zurück
+name = "Beau"
+print(name.lower())
+print("au" in name)
+
+#um " in string schreiben: \" nächster buchstage ist nicht was er normalerweiße bedeutet
+#\n zeilensprung oder ander Format sachen
+#Strings können mit [] geöffnet werden name[0:4],...
+
+done = True
+done = False
+#Großschreiben
+#1:22:41
